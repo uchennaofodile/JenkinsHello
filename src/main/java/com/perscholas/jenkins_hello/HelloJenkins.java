120d6c1c6a0e2b875dc1c791a3cb0b4b8f7bddea
Uchenna Ofodile <uchenna.ofodile@gmail.com>
2023-03-28 15:25:17 -0400

    Initial Commit

---
+++ src/main/java/com/perscholas/jenkins_hello/HelloJenkins.java
@@ -0,0 +1,18 @@
+package com.perscholas.jenkins_hello;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		System.out.println(helloJenkins());
+		System.out.println(helloJenkinsAgain());
+	}
+	
+	public static String helloJenkins() {
+		return "Hello Jenkins!";
+	}
+	
+	public static String helloJenkinsAgain() {
+		return "Hello Jenkins again!!!";
+	}
+
+}
